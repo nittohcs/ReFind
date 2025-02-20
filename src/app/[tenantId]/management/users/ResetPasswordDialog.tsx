ce87499eca4d54d110da3b26729ded9a11fc133d
Kuroda Mitsuhiko (黒田 光彦) <160799769+KurodaMitsuhiko@users.noreply.github.com>
2025-02-20 09:50:55 +0900

    ユーザーのパスワードリセットで、入力したパスワードではなくテナントごとの初期パスワードでリセットするようにした

--- src/app/[tenantId]/management/users/ResetPasswordDialog.tsx
+++ src/app/[tenantId]/management/users/ResetPasswordDialog.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import { FC, useCallback, useMemo, useState } from "react";
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import { Formik } from "formik";
-import * as yup from "yup";
+import { FC, useState } from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import { useMutation } from "@tanstack/react-query";
-import MiraCalForm from "@/components/MiraCalForm";
-import MiraCalTextField from "@/components/MiraCalTextField";
 import MiraCalLinearProgressWithLabel from "@/components/MiraCalLinearProgressWithLabel";
-import MiraCalButton from "@/components/MiraCalButton";
 import { useEnqueueSnackbar } from "@/hooks/ui";
 import { ReFindUser } from "@/types/user";
 import { adminSetUserPassword } from "@/services/AdminQueries";
-
-type FormValues = {
-    password: string,
-};
+import { useGetTenant } from "@/services/graphql";
+import { useTenantId } from "../../hook";
 
 type ResetPasswordDialogProps = {
     isOpened: boolean,
@@ -30,13 +23,8 @@ export const ResetPasswordDialog: FC<ResetPasswordDialogProps> = ({
     data,
     resetRowSelection,
 }) => {
-    const validationSchema = useMemo(() => yup.object().shape({
-        password: yup.string().required().min(8),
-    }), []);
-
-    const initialValues = useMemo(() => validationSchema.cast({
-        password: "",
-    }), [validationSchema]);
+    const tenantId = useTenantId();
+    const qTenant = useGetTenant(tenantId);
 
     const [totalCount, setTotalCount] = useState(1);
     const [currentCount, setCurrentCount] = useState(0);
@@ -45,12 +33,12 @@ export const ResetPasswordDialog: FC<ResetPasswordDialogProps> = ({
 
     const enqueueSnackbar = useEnqueueSnackbar();
     const mutation = useMutation({
-        async mutationFn(values: FormValues) {
+        async mutationFn() {
             const users = data ?? [];
             setTotalCount(users.length);
             setCurrentCount(0);
             for(const user of users) {
-                await adminSetUserPassword(user, values.password);
+                await adminSetUserPassword(user, qTenant.data!.initialPassword);
                 setCurrentCount(x => x + 1);
             }
         },
@@ -77,48 +65,38 @@ export const ResetPasswordDialog: FC<ResetPasswordDialogProps> = ({
             }
         },
     });
-    const onSubmit = useCallback((values: FormValues) => mutation.mutate(values), [mutation]);
+
+    if (!qTenant.isFetched || !qTenant.data) {
+        return null;
+    }
 
     return (
         <Dialog fullWidth maxWidth="sm" open={isOpened} onClose={() => !mutation.isPending && close()}>
             <DialogTitle>パスワードリセット</DialogTitle>
-            <Formik<FormValues>
-                validationSchema={validationSchema}
-                initialValues={initialValues}
-                onSubmit={onSubmit}
-            >
-                <MiraCalForm disablePadding disableGap>
-                    <DialogContent sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                        <DialogContentText>
-                            入力したパスワードを選択したユーザーの仮パスワードとして設定します。
-                        </DialogContentText>
-                        <MiraCalTextField
-                            name="password"
-                            label="パスワード"
-                            type="password"
-                        />
-                        {mutation.isPending && (
-                            <MiraCalLinearProgressWithLabel value={progressValue} label={progressLabel} />
-                        )}
-                    </DialogContent>
-                    <DialogActions sx={{ p: 3, pt: 0 }}>
-                        <MiraCalButton
-                            variant="contained"
-                            type="submit"
-                            disabled={mutation.isPending}
-                        >
-                            パスワード変更
-                        </MiraCalButton>
-                        <MiraCalButton
-                            variant="contained"
-                            onClick={close}
-                            disabled={mutation.isPending}
-                        >
-                            キャンセル
-                        </MiraCalButton>
-                    </DialogActions>
-                </MiraCalForm>
-            </Formik>
+            <DialogContent sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                <DialogContentText>
+                    選択したユーザーのパスワードを初期パスワードにリセットします。
+                </DialogContentText>
+                {mutation.isPending && (
+                    <MiraCalLinearProgressWithLabel value={progressValue} label={progressLabel} />
+                )}
+            </DialogContent>
+            <DialogActions sx={{ p: 3, pt: 0 }}>
+                <Button
+                    variant="contained"
+                    onClick={() => mutation.mutate()}
+                    disabled={mutation.isPending}
+                >
+                    パスワードリセット
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={close}
+                    disabled={mutation.isPending}
+                >
+                    キャンセル
+                </Button>
+            </DialogActions>
         </Dialog>
     )
 };
