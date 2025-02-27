a43d263ecf562a8ad4fc2fdbd2cfec51766fc66c
Kuroda Mitsuhiko (黒田 光彦) <160799769+KurodaMitsuhiko@users.noreply.github.com>
2025-02-27 09:45:17 +0900

    ユーザー画像をアップロードしても前の画像が表示されるので、試しにImageのunoptimizedを有効にしてみる

--- src/components/MiraCalImageUpload.tsx
+++ src/components/MiraCalImageUpload.tsx
@@ -119,6 +119,7 @@ export const MiraCalImageUpload: FC<MiraCalImageUploadProps> = ({ ...props }) =>
                         alt="現在の画像"
                         width={props.previewImageWidth || 300}
                         height={props.previewImageHeight || 300}
+                        unoptimized={true}
                     />
                 )}
                 {field.value === ImageUploadState.Upload && (
@@ -127,6 +128,7 @@ export const MiraCalImageUpload: FC<MiraCalImageUploadProps> = ({ ...props }) =>
                         alt="選択された画像"
                         width={props.previewImageWidth || 300}
                         height={props.previewImageHeight || 300}
+                        unoptimized={true}
                     />
                 )}
             </Box>
